d3b2c2290fd0365a1ccb2bd75d5389507274061e
sebslon <sebastesterprogram@gmail.com>
2021-03-30 16:18:26 +0200

    Add users controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BasketModule } from './basket/basket.module';
 import { ShopModule } from './shop/shop.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BasketModule, ShopModule],
+  imports: [BasketModule, ShopModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
